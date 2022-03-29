46ab9020dc11b27230fb3edc2507e5cb7d0fb609
Rocio Dure <rocioevelyndure@gmail.com>
2022-03-29 10:00:09 -0300

    cambios en buscar

--- src/components/Buscar.js
+++ src/components/Buscar.js
@@ -4,7 +4,7 @@ import { baseUrlApi, apiKey } from "../auxiliares/funcionesAuxiliares";
 import { useNavigate } from "react-router-dom";
 
 const Buscar = () => {
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     const [valorInput, setValorInput] = useState("")
     const [peliculas, setPeliculas] = useState([])
     const [searchParams, setSearchParams] = useSearchParams({
@@ -12,22 +12,22 @@ const Buscar = () => {
     })
 
     useEffect(() => {
-        
-        fetch(`${baseUrlApi}?api_key=${apiKey}&query=${searchParams.get('query')}&language=en-ES`)
-            .then(res => res.json())
-            .then(data => {
-                if (data.results) {
-                    setPeliculas(data.results)
-                }
-            })
-        
+        if (searchParams.get('query')) {
+            fetch(`${baseUrlApi}?api_key=${apiKey}&query=${searchParams.get('query')}&language=en-ES`)
+                .then(res => res.json())
+                .then(data => {
+                    if (data.results) {
+                        setPeliculas(data.results)
+                    }
+                })
+        }
+
     }, [searchParams])
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(valorInput)
         navigate(`/buscar?query=${valorInput}`)
-        
+
     }
 
     const handleChange = (e) => {
@@ -35,14 +35,14 @@ const Buscar = () => {
     }
 
     return (
-        <div>
-        <form onSubmit={handleSubmit}>
-            <label  >Buscar</label>
-            <input type="text" onChange={handleChange} required></input>
-            <input type="submit" value="Submit"></input>
-        </form>
-        { peliculas.map(pelicula => <h1>{pelicula.title}</h1>) }
-        </div>
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>Buscar</label>
+                <input type="text" onChange={handleChange} required></input>
+                <input type="submit" value="Submit"></input>
+            </form>
+            {peliculas.map(pelicula => <h1 key={pelicula.id}>{pelicula.title}</h1>)}
+        </>
     )
 }
 
