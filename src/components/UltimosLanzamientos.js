94a08582a2a6d578c0d3d619d7a633a17798fe8c
Rocio Dure <rocioevelyndure@gmail.com>
2022-03-29 11:25:02 -0300

    cambios en ultimos lanzamientos

--- src/components/UltimosLanzamientos.js
+++ src/components/UltimosLanzamientos.js
@@ -1,6 +1,22 @@
-const UltimosLanzamientos = () =>{
-    return(
+import { useEffect, useState } from "react";
+import { baseUrlApi, apiKey } from "../auxiliares/funcionesAuxiliares";
+
+const UltimosLanzamientos = () => {
+
+    const [peliculas, setPeliculas] = useState([])
+
+    useEffect(() => {
+        fetch(`${baseUrlApi}/movie/now_playing?api_key=${apiKey}&languaje=es-ES`)
+            .then(res => res.json())
+            .then(data => setPeliculas(data.results))
+
+    }, [])
+
+    return (
+        <>
         <h1>ultimos Lanzamientos</h1>
+        {peliculas.map(pelicula => <p key={pelicula.id}>{pelicula.title}</p>)}
+        </>
     )
 }
 
