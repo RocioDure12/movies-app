a85e74c4cfa8a21c3595659d273145d0cc8c7824
Rocio Dure <rocioevelyndure@gmail.com>
2022-04-06 11:58:09 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Populares from "./components/Populares";
 import Buscar from "./components/Buscar";
 import DetalleDePelicula from "./components/DetalleDePelicula";
 import "./styles/reset.scss";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -19,8 +20,8 @@ const App = () => {
         <Route path="/populares" element={<Populares />}></Route>
         <Route path="/buscar" element={<Buscar />}></Route>
         <Route path="/pelicula/:id" element={<DetalleDePelicula />}></Route>
-
       </Routes>
+      <Footer/>
     </BrowserRouter>
 
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import "../styles/footer.scss";
+
+const Footer = () =>{
+    return(
+        <div className="contenedor">
+            <p>icon peli</p>
+            <p>Movie App</p>
+        </div>
+    )
+}
+
+export default Footer;
